132f7adcbeb236d915bd0adb2b5c81880e8c49e6
shekhar <KUMA398513@exactsoftware.com>
2025-04-07 16:37:17 +0530

    new ui

--- src/app/itinerary-result/itinerary-result.component.ts
+++ src/app/itinerary-result/itinerary-result.component.ts
@@ -14,7 +14,9 @@ interface Itinerary {
 export class ItineraryResultComponent implements OnInit {
   itinerary: Itinerary | null = null;
   response: any;
-  result: any;
+  result = "Added flex-grow to the main container to ensure it stretches to fill the screen height dynamically. If ThreadAbortException is caught, it must be rethrown or else Monos runtime will not be able to handle it properly. This is a known issue in Mono's implementation of the ThreadAbortException. The code is using the 'gemma3:1b' model to generate an itinerary for Jaipur. The model is being called with a message that asks it to create an itinerary for Jaipur. The response from the model is being logged to the console.";
+
+
   constructor(private ollamaService: OllamaService){}
   async ngOnInit() {
     const itineraryString = localStorage.getItem('itinerary');
@@ -27,15 +29,15 @@ export class ItineraryResultComponent implements OnInit {
       const stream = false;
       const request = { model, messages, stream };
 
-      this.ollamaService.generate('gemma3:1b', 'Explain the internet').subscribe({
-        next: (data) => {
-          this.response = data;
-        },
-        error: (error) => {
-          console.error('There was an error!', error);
-        },
-      });
-      console.log('Itinerary Result:', this.result);
+      // this.ollamaService.generate('gemma3:1b', 'Explain the internet').subscribe({
+      //   next: (data) => {
+      //     this.response = data;
+      //   },
+      //   error: (error) => {
+      //     console.error('There was an error!', error);
+      //   },
+      // });
+      // console.log('Itinerary Result:', this.result);
     }
   }
 }
